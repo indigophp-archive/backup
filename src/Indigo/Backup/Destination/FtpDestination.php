43da31e1ff425a89ec892f2fc7c99bee56a7d359
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2013-12-29 19:35:46 +0100

    Working FTP

--- src/Indigo/Backup/Destination/FtpDestination.php
+++ src/Indigo/Backup/Destination/FtpDestination.php
@@ -5,86 +5,79 @@ namespace Indigo\Backup\Destination;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\Options;
+use Flysystem\Filesystem;
+use Flysystem\Adapter\Ftp as Adapter;
 
 class FtpDestination implements DestinationInterface
 {
     protected $ftp;
+    protected $connection;
     protected $options = array();
 
-    public function __construct(array $options)
+    public function __construct(array $options = array())
     {
         $resolver = new OptionsResolver();
         $this->setDefaultOptions($resolver);
 
         $options = $resolver->resolve($options);
 
-        $this->connect($options['host'], $options['port']);
-        $this->login($options['user'], $options['pass']);
-        $this->chdir($options['path']);
+        $ftp = new Filesystem(new Adapter($options));
+
+        $dir = '';
+        foreach (explode('/', trim($options['path'])) as $path) {
+            $dir .= $path . '/';
+
+            if ($ftp->has($dir)) {
+                continue;
+            }
+
+            $ftp->createDir($dir);
+        }
+
+        $this->options = $options;
+        $this->ftp = $ftp;
     }
 
     protected function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setRequired(array('user', 'pass', 'host', 'path'));
+        $resolver->setRequired(array('username', 'password', 'host', 'root', 'path'));
 
-        $resolver->setDefaults(array('port' => 21));
+        $resolver->setDefaults(array(
+            'port'    => 21,
+            'passive' => true,
+            'ssl'     => false,
+            'timeout' => 30
+        ));
 
         $resolver->setAllowedTypes(array(
-            'user' => 'string',
-            'pass' => 'string',
-            'host' => 'string',
-            'path' => 'string',
-            'port' => 'integer'
+            'username' => 'string',
+            'password' => 'string',
+            'host'     => 'string',
+            'root'     => 'string',
+            'path'     => 'string',
+            'port'     => 'integer',
+            'passive'  => 'bool',
+            'ssl'      => 'bool',
+            'timeout'  => 'integer',
         ));
 
         $resolver->setNormalizers(array(
+            'root' => function (Options $options, $value) {
+                return rtrim($value, '/') . '/';
+            },
             'path' => function (Options $options, $value) {
-                return rtrim($value) . '/';
+                return trim($value) . '/';
             },
         ));
-    }
-
-    public function __destruct()
-    {
-        ftp_close($this->ftp);
-    }
-
-    protected function connect($host, $port)
-    {
-        $this->ftp = ftp_connect($host, $port);
-
-        if ($this->ftp === false) {
-            throw new \RuntimeException('Cannot connect to FTP server: ' . $host . ':' . $port);
-        }
-
-        ftp_pasv($this->ftp, true);
-    }
-
-    protected function login($user, $pass)
-    {
-        $success = ftp_login($this->ftp, $user, $pass);
-
-        if ($success === false) {
-            throw new \InvalidArgumentException('Invalid credentials');
-        }
-    }
-
-    protected function chdir($path, $create = true)
-    {
-        if (!@ftp_chdir($this->ftp, $path)) {
-            if ($create) {
-                ftp_mkdir($this->ftp, $path);
-                $this->chdir($path, false);
-            } else {
-                throw new \RuntimeException('Cannot change to dir: ' . $path);
-            }
-        }
     }
 
     public function put(array $files)
     {
         foreach ($files as $file) {
-            ftp_put($this->ftp, basename($file), $file, FTP_BINARY);
+            $name = basename($file);
+            $file = fopen($file, 'r+');
+            $this->ftp->putStream($this->options['path'] . $name, $file);
+            fclose($file);
         }
     }
 }
